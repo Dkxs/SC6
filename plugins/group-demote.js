08cd26c10e82209699367f13221607ae872b755f
Dkxs <107620455+Dkxs@users.noreply.github.com>
2022-07-16 12:30:00 +0700

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, text, participants }) => {
+/*let handler = async (m, { conn, text, participants }) => {
 if (!text) throw `tag someone`
 let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 await conn.groupParticipantsUpdate(m.chat, [users], 'demote')
